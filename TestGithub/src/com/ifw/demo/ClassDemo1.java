ee30ec1b2e3e9ec9a457f8b7e0070d05fccc91e9
Pablo Sanmartin <pablo@aunasoft.com>
2012-02-05 05:30:39 -0500

    PSA: Test NB

--- TestGithub/src/com/ifw/demo/ClassDemo1.java
+++ TestGithub/src/com/ifw/demo/ClassDemo1.java
@@ -1,5 +1,13 @@
 package com.ifw.demo;
 
 public class ClassDemo1 {
+	public String strTest = "";
 
+	public String getStrTest() {
+		return strTest;
+	}
+
+	public void setStrTest(String strTest) {
+		this.strTest = strTest;
+	}
 }
